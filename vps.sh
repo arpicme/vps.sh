c0e67bb5090b1f7bfba36278f32dfc7dbfcff822
Apric <arpicme@yandex.ru>
2026-09-18 13:12:17 +1000

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -185,7 +185,7 @@ starship preset gruvbox-rainbow --force -o ~/.config/starship.toml
 # === 9. Обновление плагинов Zinit при запуске скрипта ===
 if command -v zsh >/dev/null 2>&1 && [ -f "$HOME/.local/share/zinit/zinit.zsh" ]; then
     echo "Обновляем плагины Zsh и Zinit..."
-    zsh -ic "source ~/.zshrc && zinit self-update && zinit update --all" || true
+    zsh -ic "source ~/.zshrc && zinit self-update -q && zinit update --all -q" || true
 fi
 
 # === 10. Завершение работы ===
